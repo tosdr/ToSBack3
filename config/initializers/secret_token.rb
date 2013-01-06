51c81b8e3b16712533316c6598de7b33898a5ac4
Jimm Stout <Jamesstout1@gmail.com>
2013-01-06 00:18:17 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tosback3::Application.config.secret_token = 'ae4a536475f56db8145beb1c25e7dce53596ce98c06422d74f7a74dd34ecd755419784e68e652a370ae5d5179414bb1bb6de6529e1e2aca9fa2c4e9b6c2dac64'
