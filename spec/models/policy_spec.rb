c4476f9aad2579c6c3551b6b31e19d937941e5ad
Jimm Stout <Jamesstout1@gmail.com>
2013-01-07 15:08:31 -0500

    add policy presence validation tests

--- spec/models/policy_spec.rb
+++ spec/models/policy_spec.rb
@@ -1,5 +1,33 @@
 require 'spec_helper'
 
 describe Policy do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    FactoryGirl.create(:policy).should be_valid
+  end
+  it "is invalid without a name" do
+    FactoryGirl.build(:policy, name: nil).should_not be_valid
+  end 
+  it "is invalid without a url" do
+    FactoryGirl.build(:policy, url: nil).should_not be_valid
+  end
+  it "is invalid without a site_id" do
+    FactoryGirl.build(:policy, site_id: nil).should_not be_valid
+  end
+  
+  # describe "with duplicate url" do
+  #   before :each do
+  #     @fb = FactoryGirl.create(:site)
+  #     @dup_site = FactoryGirl.build(:site)
+  #   end
+  #   
+  #   it "is invalid without a unique url" do
+  #     @dup_site.should_not be_valid
+  #   end
+  #   
+  #   it "is invalid if url isn't unique (case insensitive)" do
+  #     @dup_site.name.upcase!
+  #     @dup_site.should_not be_valid
+  #   end
+  # end
 end
