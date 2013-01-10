981258fc677567addb105e6d5b07fea4dec1baaf
Jimm Stout <Jamesstout1@gmail.com>
2013-01-09 23:28:10 -0500

    refactor policy spec

--- spec/models/policy_spec.rb
+++ spec/models/policy_spec.rb
@@ -5,33 +5,39 @@ describe Policy do
   it "has a valid factory" do
     FactoryGirl.create(:policy).should be_valid
   end
-  it "is invalid without a name" do
-    FactoryGirl.build(:policy, name: nil).should_not be_valid
-  end 
-  it "is invalid without a url" do
-    FactoryGirl.build(:policy, url: nil).should_not be_valid
-  end
-  it "is invalid without a site_id" do
-    FactoryGirl.build(:policy, site_id: nil).should_not be_valid
-  end
-  
-  pending "is invalid if url and xpath are duplicates"
-  pending "is invalid if the site_id and name are duplicates"
-  pending "is valid if url is duplicate but xpath is different"
-  pending "is valid if xpath is duplicate but url is different"
-  # describe "with duplicate url" do
-  #   before :each do
-  #     @fb = FactoryGirl.create(:site)
-  #     @dup_site = FactoryGirl.build(:site)
-  #   end
-  #   
-  #   it "is invalid without a unique url" do
-  #     @dup_site.should_not be_valid
-  #   end
-  #   
-  #   it "is invalid if url isn't unique (case insensitive)" do
-  #     @dup_site.name.upcase!
-  #     @dup_site.should_not be_valid
-  #   end
+  # it "is invalid without a site_id" do
+  #   FactoryGirl.build(:policy, site_id: nil).should_not be_valid
   # end
+  
+  describe "#validates" do
+    describe "attributes can't be blank" do
+      it "is invalid without a name" do
+        FactoryGirl.build(:policy, name: nil).should_not be_valid
+      end 
+      it "is invalid without a url" do
+        FactoryGirl.build(:policy, url: nil).should_not be_valid
+      end
+    end
+    
+    describe "attributes must be unique" do
+      let(:uniq) { FactoryGirl.create(:policy) }
+      let(:dup) { FactoryGirl.build(:policy) }
+    
+      context "when url and xpath are duplicates" do
+        it "is invalid" do
+          dup.should_not be_valid
+        end
+      end
+      
+      it "is valid if url is duplicate but xpath is different" do
+        dup.xpath = "//div[@id='xxxxx']"
+        dup.should be_valid
+      end
+      it "is valid if xpath is duplicate but url is different" do
+        dup.url = "http://ex.com/terms"
+        dup.should be_valid
+      end
+    end # describe unique
+  end # validates
+
 end
