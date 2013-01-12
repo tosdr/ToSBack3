a3f1814df034a870aa24df1f78708cf3294737a1
Jimm Stout <Jamesstout1@gmail.com>
2013-01-12 18:06:00 -0500

    write test specs for commitments model

--- spec/models/commitments_spec.rb
+++ spec/models/commitments_spec.rb
@@ -1,5 +1,37 @@
 require 'spec_helper'
 
 describe Commitments do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    FactoryGirl.create(:commitment).should be_valid
+  end
+  
+  describe "#validates" do
+    let(:comm) { FactoryGirl.build(:commitment) }
+    
+    it "is invalid without site_id" do
+      FactoryGirl.build(:commitment, site_id: nil)
+    end
+    
+    it "is invalid without policy_id" do
+      FactoryGirl.build(:commitment, policy_id: nil)
+    end
+    
+    context "when row exists in database" do
+      let!(:eg) { FactoryGirl.create(:commitment) }
+
+      it "is is invalid if the site and policy ids are both duplicates" do
+        comm.should_not be_valid
+      end
+      
+      it "is valid if site id is unique and policy id exists" do
+        comm.policy_id = 22
+        comm.should be_valid
+      end
+      
+      it "is valid if policy id is unique and site id exists" do
+        comm.site_id = 22
+        comm.should be_valid
+      end
+    end
+  end #validates
 end
