ff7f4fecf974bfefef10e72a2c0e701bf8ae0d96
Jimm Stout <Jamesstout1@gmail.com>
2013-01-17 14:57:13 -0500

    validate presence of Version attributes

--- app/models/policy.rb
+++ app/models/policy.rb
@@ -1,6 +1,7 @@
 class Policy < ActiveRecord::Base
   has_many :commitments
   has_many :sites, through: :commitments
+  has_many :versions
   attr_accessible :crawl, :lang, :name, :url, :xpath
   
   validates :name, :url, presence: true
--- app/models/version.rb
+++ app/models/version.rb
@@ -1,3 +1,7 @@
 class Version < ActiveRecord::Base
+  belongs_to :policy
+  
   attr_accessible :policy_id, :previous_crawl
+  
+  validates :policy_id, :previous_crawl, presence: true
 end
