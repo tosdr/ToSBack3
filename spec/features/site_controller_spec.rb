e5b59f432e1d0d9ca2cc51ee6c3e56f3bee18b93
Jimm Stout <Jamesstout1@gmail.com>
2013-02-03 16:35:55 -0500

    add links to policies from the site_path(:id) view

--- spec/features/site_controller_spec.rb
+++ spec/features/site_controller_spec.rb
@@ -53,8 +53,7 @@ describe "Site Controller" do
         end
       end
       
-      it "policy names are links to policy pages" do
-        pending("Policy controller and views need to be implemented")
+      specify "policy names are links to policy pages" do
         click_link site.policies.first.name
         page.should have_selector("h1", site.policies.first.name )
       end
