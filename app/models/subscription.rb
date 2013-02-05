0ae2f58e7b540b9dcbc34990f850c575c1a6ee86
Jimm Stout <Jamesstout1@gmail.com>
2013-02-05 12:37:10 -0500

    generate subscriptions model

---
+++ app/models/subscription.rb
@@ -0,0 +1,3 @@
+class Subscription < ActiveRecord::Base
+  attr_accessible :policy_id, :user_id
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130205164614) do
+ActiveRecord::Schema.define(:version => 20130205173331) do
 
   create_table "commitments", :force => true do |t|
     t.integer  "policy_id"
@@ -40,6 +40,13 @@ ActiveRecord::Schema.define(:version => 20130205164614) do
 
   create_table "sites", :force => true do |t|
     t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "subscriptions", :force => true do |t|
+    t.integer  "policy_id"
+    t.integer  "user_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
