c40927772b022f41d22113287e3f0293e98676c4
Jimm Stout <Jamesstout1@gmail.com>
2013-02-16 00:43:37 -0500

    assign the user in the session when login is valid and create current_user method in sessionhelper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:email].downcase)
     if user && user.authenticate(params[:password])
-      # Sign the user in and redirect to the user's show page.
+      session[:user_id] = user.id
+      redirect_to user_path(user), notice: "Successfully signed in!"
     else
       flash.now[:error] = "Sorry, the email and password combination was invalid."
       render :new
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,7 @@
 module SessionsHelper
+  
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  
 end
