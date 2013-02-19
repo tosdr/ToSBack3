5cc7f66c0ed80dd835dd22459fc81b3c75156d76
Jimm Stout <Jamesstout1@gmail.com>
2013-02-19 11:00:25 -0500

    add integration tests for user signup

--- spec/features/users_controller_spec.rb
+++ spec/features/users_controller_spec.rb
@@ -6,16 +6,39 @@ describe "UsersController" do
   describe "visiting signup_path" do
     before { visit signup_path }
     
+    it { should have_field("user_name") }
+    it { should have_field("user_email") }
+    it { should have_field("user_password") }
+    it { should have_field("user_password_confirmation") }
     it { should have_button("Create") }
     
     describe "signing up" do
+      before do
+        @user = FactoryGirl.build(:user)
+        fill_in "user_name", with: @user.name
+        fill_in "user_email", with: @user.email
+        fill_in "user_password", with: @user.password
+      end
+      
       context "with valid info" do
-        pending "creates a new user"
-        pending "automatically signs in user"
+        before do 
+          fill_in "user_password_confirmation", with: @user.password_confirmation
+          click_button "Create"
+        end
+
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out', href: signout_path) }
       end
       
       context "with invalid user info" do
-        pending "displays user.errors"
+        before do
+          fill_in "user_password_confirmation", with: @user.password_confirmation.upcase
+          click_button "Create"
+        end
+        
+        it "displays user.errors" do
+          it { should have_selector('div.alert.alert-error', text: 'error') }
+        end
       end #with invalid info
       
     end #signing up
