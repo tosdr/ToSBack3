b30f402657c08d10c07d26f946c7fe7d2f9a627b
Jimm Stout <Jamesstout1@gmail.com>
2013-02-23 17:33:17 -0500

    remove reference to old file

--- spec/support/partials_spec.rb
+++
@@ -1,9 +0,0 @@
-shared_examples "signed in header links" do
-  it { should have_link('Sign out', href: signout_path) }
-  it { should_not have_link('Sign in', href: signin_path) }
-end
-
-shared_examples "signed out header links" do
-  it { should_not have_link('Sign out', href: signout_path) }
-  it { should have_link('Sign in', href: signin_path) }
-end
