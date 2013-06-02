5a830ff24c5cc7db90a63dcf20043111375dce5b
Jimm Stout <jamesstout1@gmail.com>
2013-06-02 19:15:11 +0200

    add some pending tests for crawls controller

--- spec/features/crawl_controller_spec.rb
+++ spec/features/crawl_controller_spec.rb
@@ -4,16 +4,37 @@ describe "CrawlController" do
   before { @crawl = FactoryGirl.create(:crawl) }
   subject { page }
 
+  context "as an admin" do
+    before(:each) do
+      @admin = FactoryGirl.create(:admin)
+      sign_in @admin
+    end
 
-  describe "visiting crawl #show" do
-    before { visit crawl_path(@crawl) }
-    
-    context "as an admin" do
-      it { should have_selector('h2', text: @crawl.created_at) }
+    describe "visiting crawl #show" do
+      before { visit crawl_path(@crawl) }
+      
+      xit { has_selector?('h2', text: @crawl.policy.name) }
+      xit { has_selector?('div#crawled_policy', text: @crawl.crawled_policy) }
+      xit { has_button?('Approve!') }
+      xit { has_button?('Skip!') }
+      xit { has_button?('Revise...') }
+        
+    end #visiting crawl #show
+  end #as an admin
+  
+  context "as regular user" do
+    before(:each) do
+      @user = FactoryGirl.create(:user)
+      sign_in @user
     end
     
-    context "as regular user" do
-      pending "redirects to policy"
-    end 
-  end #visiting crawl #show
-end
+    describe "visiting crawl_path" do
+      before { visit crawl_path(@crawl) }
+      
+      xit "redirects to policy path" do
+        current_path.should eq(policy_path(@crawl.policy))
+      end
+    end #visiting crawl_path
+    
+  end #as user
+end # CrawlController
