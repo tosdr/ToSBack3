100946a0084f9dd887aca384994ebfe3fe558fbe
Jimmy Stout <jamesstout1@gmail.com>
2019-04-09 13:47:11 -0400

    add link to user account settings

--- spec/support/partials_shared.rb
+++ spec/support/partials_shared.rb
@@ -1,7 +1,7 @@
 RSpec.shared_examples "it has signed in header links" do
   it { should have_link('Sign out', href: destroy_user_session_path) }
-  xit { should have_link('Subscriptions', href: user_path(user_id))}
-  xit { should have_link('Account', href: edit_user_path(user_id))}
+  xit { should have_link('Subscriptions', href: user_path(user_id)) }
+  it { should have_link('Account', href: edit_user_registration_path) }
   it { should_not have_link('Sign in', href: new_user_session_path) }
 end
 
